17b4978ed9fa98d413e6ac12ce413cbdd7b4cf24
J-sense <jishan1873@gmail.com>
2024-12-07 22:10:12 +0600

    ok

--- src/modules/product/bike.router.ts
+++ src/modules/product/bike.router.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import { bikeController } from './bike.controller';
 
 const router = express.Router();
-router.post('/created-bike', bikeController.createBike);
+router.post('/products', bikeController.createBike);
 router.get('/products', bikeController.findall);
-router.get('/:id', bikeController.findOneProduct);
-router.put('/:id', bikeController.updateProduct);
-router.delete('/:id', bikeController.deleteProduct);
+router.get('/products/:id', bikeController.findOneProduct);
+router.patch('/products/:id', bikeController.updateProduct);
+router.delete('/products/:id', bikeController.deleteProduct);
 
 export const bikeroutes = router;
